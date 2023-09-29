38f8cb0aa43b284ecdee6efed914e957c500f240
Jan Dostal <jan.dostal@tul.cz>
2023-09-29 21:22:07 +0200

    Completed first exercise

--- Exercise_01/Main.c
+++ Exercise_01/Main.c
@@ -2,83 +2,59 @@
 
 #include <stdio.h>
 
-int test_bin_values(FILE* file, int expected_int, float expected_float, double expected_double)
+int binftest(char* fileName, int expectedInt, float expectedFloat, double expectedDouble)
 {
-    if (file == NULL)
-    {
-        printf("Soubor bin chyba");
-        return 1;
-    }
+    FILE* bin;
 
-    fseek(file, 0, SEEK_SET);
+    bin = fopen(fileName, "rb");
+
+    if (bin == NULL) return 1;
+
+    fseek(bin, 0, SEEK_SET);
 
     int a = 0;
     float b = 0;
     double c = 0;
 
-    fread(&a, sizeof(int), 1, file);
-    fread(&b, sizeof(float), 1, file);
-    fread(&c, sizeof(double), 1, file);
+    fread(&a, sizeof(int), 1, bin);
+    fread(&b, sizeof(float), 1, bin);
+    fread(&c, sizeof(double), 1, bin);
 
-    fclose(file);
+    printf("Nactena data z binarniho souboru: a = %d, b = %f, c = %lf \n", a, b, c);
 
-    printf("Nactena data: a = %d, b = %f, c = %f \n", a, b, c);
+    fclose(bin);
 
-    if (expected_int != a)
-    {
-        printf("Neshoduje se int \n");
-        return 2;
-    }
-    else if (expected_float != b)
-    {
-        printf("Neshoduje se float \n");
-        return 2;
-    }
-    else if (expected_double != c)
-    {
-        printf("Neshoduje se double \n");
-        return 2;
-    }
+    if (expectedInt != a) return 2;
 
-    printf("Data se shoduji \n");
+    if (expectedFloat != b) return 3;
+
+    if (expectedDouble != c) return 4;
 
     return 0;
 }
 
-int test_text_values(FILE* file, int expected_int, float expected_float, double expected_double)
+int txtftest(char* fileName, int expectedInt, float expectedFloat, double expectedDouble)
 {
-    if (file == NULL)
-    {
-        printf("Soubor text chyba");
-        return 1;
-    }
+    FILE* text;
+
+    text = fopen(fileName, "r");
+
+    if (text == NULL) return 1;
 
     int a = 0;
     float b = 0;
     double c = 0.0;
 
-    while (fscanf(file, "%d %f %f", &a, &b, &c) != EOF)
-        printf("Nactena data: %d %f %f \n", a, b, c);
+    while (fscanf(text, "%d %f %lf", &a, &b, &c) != EOF)
+        printf("Nactena data z textoveho souboru: a = %d, b = %f, c = %lf \n", a, b, c);
 
-    fclose(file);
+    fclose(text);
 
-    if (expected_int != a)
-    {
-        printf("Neshoduje se int \n");
-        return 2;
-    }
-    else if (expected_float != b)
-    {
-        printf("Neshoduje se float \n");
-        return 2;
-    }
-    else if (expected_double != c)
-    {
-        printf("Neshoduje se double \n");
-        return 2;
-    }
-
-    printf("Data se shoduji \n");
+    if (expectedInt != a) return 2;
+    
+    if (expectedFloat != b) return 3;
+ 
+    if (expectedDouble != c) return 4;
 
     return 0;
 }
@@ -87,40 +63,89 @@ int main()
 {
     // Write C code here
 
-    FILE* bin;
-    FILE* text;
-
-    bin = fopen("SOUBOR.DAT", "wb+");
-    text = fopen("SOUBOR.TXT", "w+");
-
-    if (bin == NULL)
-    {
-        printf("chyba binarni");
-        return 1;
-    }
-
-    if (text == NULL)
-    {
-        printf("chyba text");
-        return 2;
-    }
-
     int a = 0;
     float b = 0;
     double c = 0.0;
 
     printf("Zadejte 3 cisla a stisknete enter: \n");
-    scanf("%d %f %f", &a, &b, &c);
+    scanf("%d %f %lf", &a, &b, &c);
 
-    fprintf(text, "%d %f %f \n", a, b, c);
+    printf("Zadana cisla: a = %d, b = %f, c = %lf \n", a, b, c);
+
+    FILE* bin;
+    FILE* text;
+
+    char* binaryFileName = "SOUBOR.DAT";
+    char* textFileName = "SOUBOR.TXT";
+
+    bin = fopen(binaryFileName, "wb");
+
+    if (bin == NULL)
+    {
+        printf("Chyba pri otevirani binarniho souboru %s pro zapis \n", binaryFileName);
+        return 1;
+    }
+
+    text = fopen(textFileName, "w");
+
+    if (text == NULL)
+    {
+        printf("Chyba pri otevirani textoveho souboru %s pro zapis \n", textFileName);
+        fclose(bin);
+        return 2;
+    }
+
+    fprintf(text, "%d %f %lf \n", a, b, c);
+
+    fclose(text);
 
     fwrite(&a, sizeof(int), 1, bin);
     fwrite(&b, sizeof(float), 1, bin);
     fwrite(&c, sizeof(double), 1, bin);
 
-    int info_text_code = test_text_values(text, a, b, c);
+    fclose(bin);
 
-    int info_bin_code = test_bin_values(bin, a, b, c);
+    int txtFileResult = txtftest(textFileName, a, b, c);
+
+    switch (txtFileResult)
+    {
+        case 0:
+            printf("Prectena data v textovem souboru se shoduji \n");
+            break;
+        case 1:
+            printf("Chyba pri otevirani textoveho souboru %s pro cteni \n", textFileName);
+            break;
+        case 2:
+            printf("V textovem souboru se neshoduje int, tedy promenna a \n");
+            break;
+        case 3:
+            printf("V textovem souboru se neshoduje float, tedy promenna b \n");
+            break;
+        case 4:
+            printf("V textovem souboru se neshoduje double, tedy promenna c \n");
+            break;
+    }
+
+    int binFileResult = binftest(binaryFileName, a, b, c);
+
+    switch (txtFileResult)
+    {
+        case 0:
+            printf("Prectena data v binarnim souboru se shoduji \n");
+            break;
+        case 1:
+            printf("Chyba pri otevirani binarniho souboru %s pro cteni \n", binaryFileName);
+            break;
+        case 2:
+            printf("V binarnim souboru se neshoduje int, tedy promenna a \n");
+            break;
+        case 3:
+            printf("V binarnim souboru se neshoduje float, tedy promenna b \n");
+            break;
+        case 4:
+            printf("V binarnim souboru se neshoduje double, tedy promenna c \n");
+            break;
+    }
 
     return 0;
 }
